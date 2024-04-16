da08757a1cb3fa4f78f081087fb2f5b3336b75c8
kanzakisuemi <jsuemikanzaki@gmail.com>
2024-04-16 01:54:30 -0300

    implements more tests and new method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   enum role: [:business_owner, :client]
+
+  def humanized_role
+    I18n.t("activerecord.attributes.user.roles.#{role}", user: self)
+  end
 
   private
 
@@ -13,4 +15,5 @@ class User < ApplicationRecord
       [I18n.t("activerecord.attributes.user.roles.#{role}"), role]
     end
   end
+
 end
--- spec/system/devise/user_logs_in_spec.rb
+++ spec/system/devise/user_logs_in_spec.rb
@@ -16,4 +16,22 @@ describe 'User logs in' do
     expect(page).to have_content('Login efetuado com sucesso.')
     expect(page).to have_content('Dono de Buffet')
   end
+  it 'and logs out' do
+    User.create!(name: 'Kylie Kristen Jenner', email: 'khy@jenner.com', password: 'password123', role: 0)
+
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'khy@jenner.com'
+      fill_in 'Senha', with: 'password123'
+      click_on 'Entrar'
+    end
+
+    within('nav') do
+      click_on 'Sair'
+    end
+
+    expect(page).to have_content('Entrar')
+    expect(page).to have_content('Logout efetuado com sucesso.')
+  end
 end
