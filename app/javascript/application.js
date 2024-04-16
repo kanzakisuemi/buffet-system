682165f1c60a0cbea47caaa9d9a4807df0249483
kanzakisuemi <jsuemikanzaki@gmail.com>
2024-04-16 18:42:23 -0300

    implements modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as Popper from "@popperjs/core"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:frame-missing", (event) => {
+  const { detail: { response, visit } } = event
+  event.preventDefault()
+  visit(response)
+})
